4c1b3956e7f25753347bf3b092b16610f7197043
tiagosil <tabss1@iscte-iul.pt>
2021-03-30 23:04:58 +0100

    excell

--- projetocodesemells/src/main/java/codesmell/writer/ExcelWriter.java
+++ projetocodesemells/src/main/java/codesmell/writer/ExcelWriter.java
@@ -1,12 +1,35 @@
 package codesmell.writer;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+
 import codesmell.report.Report;
 
 public class ExcelWriter implements IWriter {
 
 	@Override
 	public void write(Report report) {
-		// TODO Auto-generated method stub
+		List<Row> listRows = new ArrayList<Row>();
+		
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		
+		//Nome da página do excell
+		HSSFSheet sheet = workbook.createSheet("Code Semells"); 
+		
+		//primeira linha 
+		HSSFRow rowhead = sheet.createRow((short)0);
+		
+		
+		
+		
+		
 		
 	}
 
