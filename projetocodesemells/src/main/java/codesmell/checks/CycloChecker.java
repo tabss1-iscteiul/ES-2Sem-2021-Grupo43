aadb332a7d9779b34b376661ab5b4b6617245b22
tiagosil <tabss1@iscte-iul.pt>
2021-04-01 02:10:12 +0100

    cyclo

--- projetocodesemells/src/main/java/codesmell/checks/CycloChecker.java
+++ projetocodesemells/src/main/java/codesmell/checks/CycloChecker.java
@@ -2,6 +2,7 @@ package codesmell.checks;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.SimpleName;
 
 import codesmell.report.ReportEntry;
 
@@ -11,9 +12,10 @@ public class CycloChecker implements IChecker{
 	@Override
 	public ReportEntry check(CompilationUnit compilationUnit) {
 		//compilationUnit.findAll()
-		
-		compilationUnit.findAll(MethodDeclaration.class);
-		return null;
+	/*	
+	compilationUnit.findAll(MethodDeclaration.class).stream()
+		.forEach(m -> m.getBody().filter( f -> f.isWhileStmt()));
+		*/return null;
 	}
 	
 
