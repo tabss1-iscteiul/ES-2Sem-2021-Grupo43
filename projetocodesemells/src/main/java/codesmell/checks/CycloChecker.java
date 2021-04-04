228892a5d0f918ef26bd31a81e47de285ac46f51
tiagosil <tabss1@iscte-iul.pt>
2021-04-04 21:17:02 +0100

    cyclo metrix class

--- projetocodesemells/src/main/java/codesmell/checks/CycloChecker.java
+++ projetocodesemells/src/main/java/codesmell/checks/CycloChecker.java
@@ -1,39 +1,28 @@
 package codesmell.checks;
 
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.stmt.Statement;
-import com.github.javaparser.ast.stmt.SwitchStmt;
-
 import codesmell.report.ReportEntry;
 
 
-public class CycloChecker implements IChecker{
+public class CycloChecker implements IChecker {
 	private static final String CHECKER_NAME = "CycloChecker";
-	private int counter =0;
+	//private int counter = 0;
+
 	@Override
 	public ReportEntry check(CompilationUnit compilationUnit) {
-		compilationUnit.findAll(Statement.class).forEach(statement ->{
-				if (statement.isIfStmt()) {
-					counter ++;
-				}
-				if (statement.isWhileStmt()) {
-					counter ++;
-				}
-				if (statement.isForStmt()) {
-					counter ++;
-				}
-				if (statement.isSwitchStmt()) {
-					counter += statement.asSwitchStmt().getEntries().size();
-				}
-		});
-		
-	return new ReportEntry(CHECKER_NAME, String.valueOf(counter));
-	}
-	public String getCycloChecker() {
-		return CHECKER_NAME;
+		int counter = compilationUnit.findAll(Statement.class).stream().mapToInt(statement -> {
+			if (statement.isIfStmt() || statement.isWhileStmt() || statement.isForStmt() || statement.isForEachStmt()
+					|| statement.isDoStmt()) {
+				return 1;
+
+			} else if (statement.isSwitchStmt()) {
+				return statement.asSwitchStmt().getEntries().size();
+			}
+			return 0;
+		}).sum();
+
+		return new ReportEntry(CHECKER_NAME, String.valueOf(counter + 1));
 	}
 
-	
 }
