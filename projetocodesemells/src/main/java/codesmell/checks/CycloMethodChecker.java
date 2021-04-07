7a6fdd328b258c56542197a53ea4df22b414edf8
jgordo18 <jprgo1@iscte-iul.pt>
2021-04-07 12:45:13 +0100

    Métodos ciclomáticos

--- projetocodesemells/src/main/java/codesmell/checks/CycloMethodChecker.java
+++ projetocodesemells/src/main/java/codesmell/checks/CycloMethodChecker.java
@@ -8,6 +8,7 @@ import com.github.javaparser.ast.stmt.Statement;
 import codesmell.report.ReportMultipleEntry;
 
 public class CycloMethodChecker implements IChecker {
+	//Métodos Ciclomáticos 
 	private static final String CHECKER_NAME = "CycloMethodChecker";
 
 	public ReportMultipleEntry check(CompilationUnit compilationUnit) {
