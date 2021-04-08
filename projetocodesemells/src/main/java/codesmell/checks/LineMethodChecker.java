538d5bc585d534594846b31db7972082fb7a0dc4
tiagosil <tabss1@iscte-iul.pt>
2021-04-08 15:19:26 +0100

    count lines without comments

--- projetocodesemells/src/main/java/codesmell/checks/LineMethodChecker.java
+++ projetocodesemells/src/main/java/codesmell/checks/LineMethodChecker.java
@@ -3,6 +3,7 @@ package codesmell.checks;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.stmt.BlockStmt;
 
 import codesmell.report.IReportEntry;
@@ -21,7 +22,16 @@ public class LineMethodChecker implements IChecker {
 		compilationUnit.findAll(MethodDeclaration.class).stream().forEach(method -> {
 			String nameMethod = method.getNameAsString();
 
-			int result = method.toString().split("\n").length;
+		int counter = method.findAll(Comment.class).stream().mapToInt(comment ->{
+				
+				if (comment.isLineComment() || comment.isBlockComment()) {
+					return 1;
+				}else {
+					return 0;
+				}
+			}).sum();
+			
+				int result = method.toString().split("\n").length - counter;
 
 			en.addEntry(nameMethod, String.valueOf(result));
 		});
