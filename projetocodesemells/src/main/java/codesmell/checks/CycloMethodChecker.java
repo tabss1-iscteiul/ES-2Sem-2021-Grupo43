a517b82b19ee20f5ef66313f73907578ffda6645
jgordo18 <jprgo1@iscte-iul.pt>
2021-04-08 11:52:08 +0100

    alterações

--- projetocodesemells/src/main/java/codesmell/checks/CycloMethodChecker.java
+++ projetocodesemells/src/main/java/codesmell/checks/CycloMethodChecker.java
@@ -8,11 +8,9 @@ import com.github.javaparser.ast.stmt.Statement;
 import codesmell.report.ReportMultipleEntry;
 
 public class CycloMethodChecker implements IChecker {
-<<<<<<< HEAD
-	
-=======
+
 	//Métodos Ciclomáticos
->>>>>>> branch 'master' of https://github.com/tabss1-iscteiul/ES-2Sem-2021-Grupo43
+
 	private static final String CHECKER_NAME = "CycloMethodChecker";
 
 	public ReportMultipleEntry check(CompilationUnit compilationUnit) {
