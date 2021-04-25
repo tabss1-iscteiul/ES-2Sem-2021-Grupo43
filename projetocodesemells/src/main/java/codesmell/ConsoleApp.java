56533f66313141a5d87e582bd42b7bbfc21d4cb1
arsan1-iscte <arsan1@iscte-iul.pt>
2021-04-25 21:21:03 +0100

    gui_ligadaMain

--- projetocodesemells/src/main/java/codesmell/ConsoleApp.java
+++ projetocodesemells/src/main/java/codesmell/ConsoleApp.java
@@ -23,6 +23,32 @@ public class ConsoleApp {
 
 	public static void main(String[] args) throws IOException {
 		
+		   try {
+	            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+	                if ("Nimbus".equals(info.getName())) {
+	                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+	                    break;
+	                }
+	            }
+	        } catch (ClassNotFoundException ex) {
+	            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (InstantiationException ex) {
+	            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (IllegalAccessException ex) {
+	            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+	            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        }
+	        //</editor-fold>
+	        //</editor-fold>
+
+	        /* Create and display the form */
+	        java.awt.EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	                new Interface().setVisible(true);
+	            }
+	        });
+		
 		// Setup components
 		Collection<IChecker> checkers = Arrays.asList( 
 			new LineChecker(), 
