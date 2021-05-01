0c0f89020172113058fb3ee4be299fbd14ba8a3a
tiagosil <tabss1@iscte-iul.pt>
2021-05-02 00:01:44 +0100

    interface

--- projetocodesemells/src/main/java/codesmell/Interface.java
+++ projetocodesemells/src/main/java/codesmell/Interface.java
@@ -1,6 +1,8 @@
 
 package codesmell;
 
+import javax.swing.JFileChooser;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
