d0af3ebee270f5b74001509e4be8991b89091cd7
jgordo18 <jprgo1@iscte-iul.pt>
2021-05-03 20:02:11 +0100

    comentários

--- projetocodesemells/src/main/java/codesmell/writer/IWriter.java
+++ projetocodesemells/src/main/java/codesmell/writer/IWriter.java
@@ -2,7 +2,7 @@ package codesmell.writer;
 
 import codesmell.report.Report;
 
-public interface IWriter {
+public interface IWriter {// para escrever apenas um excelwriter e consolewriter
 
 	void write(Report report);
 	
