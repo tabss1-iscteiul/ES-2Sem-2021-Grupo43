57d14f542fbd984990bf1fb7e79002aaa3a9dddb
jgordo18 <jprgo1@iscte-iul.pt>
2021-05-03 18:55:22 +0100

    comentarios

--- projetocodesemells/src/main/java/codesmell/checks/CycloChecker.java
+++ projetocodesemells/src/main/java/codesmell/checks/CycloChecker.java
@@ -1,12 +1,12 @@
 package codesmell.checks;
 
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.CompilationUnit;//ir ao interior da classe
+import com.github.javaparser.ast.stmt.Statement;//vê se é um if,while 
 import codesmell.report.ReportEntry;
 
 public class CycloChecker implements IChecker {
 
-
+ //utilizacao da biblioteca javaparser - calculo de ifs,else etc...
 	private static final String CHECKER_NAME = "WMC_class";
 
 	@Override
@@ -17,7 +17,7 @@ public class CycloChecker implements IChecker {
 				return 1;
 
 			} else if (statement.isSwitchStmt()) {
-				return statement.asSwitchStmt().getEntries().size();
+				return statement.asSwitchStmt().getEntries().size();//numero de cases de switches
 			}
 			return 0;
 
