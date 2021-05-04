a76b2d62e5bf3d8c4eda6d2d2dc15af056948485
arsan1-iscte <arsan1@iscte-iul.pt>
2021-05-04 22:28:59 +0100

    writer_vFinal

--- projetocodesemells/src/main/java/codesmell/txt/WriterTXT.java
+++ projetocodesemells/src/main/java/codesmell/txt/WriterTXT.java
@@ -4,32 +4,40 @@ import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 
 
-
 public class WriterTXT {
 
+	public void write (int nomC, String regra1, int locC, String regra2, int wmcC, int locM, String regra3, int cycloM) {
+		 try {
+		      FileWriter myWriter = new FileWriter("C:\\Users\\ana_r\\Desktop\\filename.txt"); //indicar depois o caminho
+		      myWriter.write(nomC + "\n");
+		      myWriter.write(regra1 + "\n");
+		      myWriter.write(locC + "\n");
+		      myWriter.write(regra2 + "\n");
+		      myWriter.write(wmcC + "\n");
+		      
+		      
+		      myWriter.write(locM +  "\n");
+		      myWriter.write(regra3 + "\n");
+		      myWriter.write(cycloM +  "\n");
+		      myWriter.close();
+		      
+		      System.out.println("Successfully wrote to the file.");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		
+	}
+	
 	
 	  public static void main(String[] args) {
-	    try {
-	      FileWriter myWriter = new FileWriter("filename.txt");
-	      myWriter.write("Numero NOM_CLASS\n");
-	      myWriter.write("AND\n");
-	      myWriter.write("Numero LOC_CLASS\n");
-	      myWriter.write("AND\n");
-	      myWriter.write("Numero WMC_CLASS\n");
-	      
-	      
-	      myWriter.write("Numero LOC_METHOD\n");
-	      myWriter.write("AND\n");
-	      myWriter.write("Numero CYCLO_METHOD\n");
-	      myWriter.close();
-	      
-	      System.out.println("Successfully wrote to the file.");
-	    } catch (IOException e) {
-	      System.out.println("An error occurred.");
-	      e.printStackTrace();
-	    }
+	   WriterTXT writeT = new WriterTXT ( );
+	   writeT.write(10, "AND", 10, "AND", 10, 10, "AND", 10);
 	  }
 	
+	
+
 	
 
 
